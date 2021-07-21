008c2812b18abcaad11d74606debddfcd7a6e6a7
Sharon Cherop <sh.cherop@gmail.com>
2021-07-21 23:01:11 +0300

    jquery example wrong password alert

---
+++ js/script.js
@@ -0,0 +1,15 @@
+<script type="text/js">
+        $(function ()
+        
+        $("#formName_error_message").hide();
+        $("#email_error_message").hide();
+        $("#pword_error_message").hide();
+        $("#pword1_error_message").hide();
+
+        
+        )
+    </script>
+    var error_formName=false;
+    var error_eMail=false;
+    var error_password=false;
+    var error_pword=false;
